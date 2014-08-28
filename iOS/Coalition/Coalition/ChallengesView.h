1285de14f3ac668b53cb8cc7b9d574ae4bd387e7
Ibraheem AJ <gai10@uni.brighton.ac.uk>
2014-08-28 20:21:35 +0100

    Added challengesView Section

---
+++ iOS/Coalition/Coalition/ChallengesView.h
@@ -0,0 +1,13 @@
+//
+//  ChallengesView.h
+//  Coalition
+//
+//  Created by AJ Ibraheem on 28/08/2014.
+//  Copyright (c) 2014 James Nocentini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChallengesView : UIView
+
+@end
